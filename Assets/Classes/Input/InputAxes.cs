70661a451e74e15ef41123e91e9d8859e4c37e6b
Jprummel <jordiprummel@live.nl>
2016-05-26 10:25:28 +0200

    Controller inputs

---
+++ Assets/Classes/Input/InputAxes.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputAxes{
+    //Face Buttons
+    public const string A       = "Xbox_A";
+    public const string B       = "Xbox_B";
+    public const string X       = "Xbox_X";
+    public const string Y       = "Xbox_Y";
+    //Start and back
+    public const string START   = "Xbox_Start";
+    public const string BACK    = "Xbox_Back";
+    //Triggers and Bumpers
+    public const string LT      = "Xbox_LT";
+    public const string RT      = "Xbox_RT";
+    public const string LB      = "Xbox_LB";
+    public const string RB      = "Xbox_RB";
+    //Analog Clicks
+    public const string L3      = "Xbox_L3";
+    public const string R3      = "Xbox_R3";
+    //Analog Sticks
+    //Left
+    public const string LEFTX   = "Xbox_LeftStick_X";
+    public const string LEFTY   = "Xbox_LeftStick_Y";
+    //Right
+    public const string RIGHTX  = "Xbox_RightStick_X";
+    public const string RIGHTY  = "Xbox_RightStick_Y";
+    //Dpad
+    public const string DPADX   = "Xbox_DPad_X";
+    public const string DPADY   = "Xbox_DPad_Y";
+
+    //Keyboard
+    public const string KEYX    = "Horizontal";
+    public const string KEYY    = "Vertical";
+    public const string MOUSEX  = "Mouse_X";
+    public const string MOUSEY  = "Mouse_Y";
+}
---
+++ Assets/Classes/Input/PlayerInputs.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputs : MonoBehaviour {
+
+   
+
+    void Start()
+    {
+
+    }
+
+	void Update () 
+    {
+        XboxControllerInput();
+    }
+
+
+    void XboxControllerInput()
+    {
+        //DPAD
+        float dpadX = Input.GetAxis(InputAxes.DPADX); //DPAD X AXIS
+
+        if (dpadX > 0)
+        {
+
+        }
+        else if (dpadX < 0)
+        {
+
+        }
+
+        float dpadY = Input.GetAxis(InputAxes.DPADY); //DPAD Y AXIS
+
+        if (dpadY > 0)
+        {
+
+        }
+        else if (dpadY < 0)
+        {
+
+        }
+
+        //ANALOG STICKS
+        float leftX = Input.GetAxis(InputAxes.LEFTX); //LEFT ANALOG X AXIS
+        float leftY = Input.GetAxis(InputAxes.LEFTY); //LEFT ANALOG Y AXIS
+
+        Vector3 inputVector = new Vector3(Input.GetAxis(InputAxes.LEFTX),0, -Input.GetAxis(InputAxes.LEFTY));
+
+        if (leftX != 0 || leftY != 0)
+        {
+           
+        }
+
+        float rightX = Input.GetAxis(InputAxes.RIGHTX); //RIGHT ANALOG X AXIS
+        float rightY = Input.GetAxis(InputAxes.RIGHTY); //RIGHT ANALOG X AXIS
+        
+        if (rightX != 0)
+        {
+            
+        }
+
+        if (rightY != 0)
+        {
+            
+        }
+
+        if (Input.GetButtonDown(InputAxes.L3))
+        {
+
+        }
+
+        if (Input.GetButtonDown(InputAxes.R3))
+        {
+
+        }
+
+        //FACE BUTTONS
+        if (Input.GetButtonDown(InputAxes.A))
+        {
+
+        }
+
+        if (Input.GetButtonDown(InputAxes.B))
+        {
+
+        }
+        
+        if (Input.GetButtonDown(InputAxes.X))
+        {
+         
+        }
+        if (Input.GetButtonDown(InputAxes.Y))
+        {
+
+        }
+
+        //BUMPERS & TRIGGERS
+
+        //BUMPERS
+        if (Input.GetButton(InputAxes.LB))
+        {
+
+        }
+        if (Input.GetButtonDown(InputAxes.RB))
+        {
+
+        }
+
+        //TRIGGERS
+        float leftTrigger = Input.GetAxis(InputAxes.LT);
+        float rightTrigger = Input.GetAxis(InputAxes.RT);
+
+        if (leftTrigger > 0)
+        {
+
+        }
+        if (rightTrigger > 0)
+        {
+
+        }
+
+        //START & BACK
+        if (Input.GetButtonDown(InputAxes.START))
+        {
+
+        }
+        if (Input.GetButtonDown(InputAxes.BACK))
+        {
+
+        }
+
+        //Idle
+        if (!Input.anyKeyDown && leftY == 0 && leftX == 0)
+        {
+
+        }
+    }
+}
