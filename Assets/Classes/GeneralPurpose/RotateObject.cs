f7a809765eb00100ce672940d90e0d834722946e
Jprummel <jordiprummel@live.nl>
2016-05-26 11:26:52 +0200

    Object rotation script

---
+++ Assets/Classes/GeneralPurpose/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+    [SerializeField]private float _rotateX;
+    [SerializeField]private float _rotateY;
+    [SerializeField]private float _rotateZ;
+
+	void Update () {
+        RotatePlatform();
+	}
+
+    void RotatePlatform()
+    {
+        transform.Rotate(new Vector3(_rotateX, _rotateY, _rotateZ)* Time.deltaTime);
+    }
+}
