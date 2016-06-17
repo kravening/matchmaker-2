9ac3fd791a13c736eaa8c67ebf1d3065ee8fc4af
kaj loevesijn <sep_ul_tura@live.nl>
2016-06-17 22:52:26 +0200

    More Level stuff

--- Assets/Classes/Player/Collectible.cs
+++ Assets/Classes/Player/Collectible.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class Collectible : MonoBehaviour
 	{
-	public Text countText;
+	[SerializeField]private Text countText;
 	private int count;
 	private int nrOfTotalCollectables;
 	private int nrOfCollectedItems;
@@ -14,20 +14,19 @@ public class Collectible : MonoBehaviour
 		GameObject[] collectables = GameObject.FindGameObjectsWithTag("Collectable");
 		nrOfTotalCollectables = collectables.Length;
 		nrOfCollectedItems = 0;
-		Debug.Log (nrOfTotalCollectables);
 		count = 0;
 		UpdateUI();
 	}
 	public void AddCollectable()
 	{
 		nrOfCollectedItems++;
-		Debug.Log ("You have " + nrOfCollectedItems + " of the " + nrOfTotalCollectables);
+
 		count = count + 1;
 		UpdateUI();
 
 	}
 	void UpdateUI()
 	{
-		countText.text = "Score:" + count.ToString ();
+		countText.text = count.ToString ();
 	}
 }
--- Assets/Classes/Player/PlayerCollisions.cs
+++ Assets/Classes/Player/PlayerCollisions.cs
@@ -5,7 +5,7 @@ public class PlayerCollisions : MonoBehaviour {
 	private Vector3 _collidingObjectPos;
 	void Update(){
 		RaycastHit hitInfo;
-		if (Physics.Raycast (transform.position, Vector3.down, out hitInfo, 1f) && _collidingObjectPos != hitInfo.transform.position) { // makeshift onCollisionEnter
+		if (Physics.Raycast (transform.position, Vector3.down, out hitInfo, 3f) && _collidingObjectPos != hitInfo.transform.position) { // makeshift onCollisionEnter
 			_collidingObjectPos = hitInfo.transform.position;
 			if(hitInfo.transform.tag == Tags.FALLINGPLATFORM){
 				FallingPlatform fallingPlatform = hitInfo.transform.GetComponent<FallingPlatform> ();
@@ -14,16 +14,5 @@ public class PlayerCollisions : MonoBehaviour {
 		}else{
 			_collidingObjectPos = new Vector3(0,0,0);
 		}
-
 	}
-	void OnCollisionEnter(Collision coll)
-    {
-		Debug.Log (coll.gameObject.tag);
-		if (coll.gameObject.tag == Tags.FALLINGPLATFORM)
-        {
-			Debug.Log ("AAAAAAAAAAAAAAAAAAAAA");
-			FallingPlatform fallingPlatform = coll.gameObject.GetComponent<FallingPlatform>();
-			fallingPlatform.StartFall();
-        }
-    }
 }
